0458d42f9a9ba570671d00d3bd000e623e346d43
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2018-05-03 20:43:08 +0200

    [Core] Added address types feature

---
+++ Assets/_VirtualMachine/_AddressTypes/AddressTypes.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VirtualMachine
+{
+
+    public static class AddressTypes
+    {
+        private static Dictionary<char, AddressTypeBase> m_AddressTypesBySymbol;
+        private static Dictionary<long, AddressTypeBase> m_AddressTypesBytBitset;
+        private static AddressTypeBase m_DefaultAddressType;
+
+        private static void BuildAddressTypesDatabase()
+        {
+            var defaultAddressTypeType = typeof(ImmediateAddressType);//TODO:SZ - default address type
+
+            m_AddressTypesBySymbol = new Dictionary<char, AddressTypeBase>();
+            m_AddressTypesBytBitset = new Dictionary<long, AddressTypeBase>();
+
+            var addressTypes = Utilities.Reflection.GetSubclasses(typeof(AddressTypeBase), false);
+
+            foreach(var addressTypeType in addressTypes)
+            {
+                var addressType = Utilities.Reflection.CreateObjectOfType<AddressTypeBase>(addressTypeType.Name);
+                if(null == addressType)
+                    throw new System.Exception("[AddressTypes] Invalid type (" + addressTypeType + ")!");
+
+                if(m_AddressTypesBySymbol.ContainsKey(addressType.Symbol))
+                    throw new System.Exception("[AddressTypes] Duplicated symbol (" + addressType.Symbol + ") of types (" + addressTypeType + ") and (" + m_AddressTypesBySymbol[addressType.Symbol].GetType().Name + ")!");
+
+                m_AddressTypesBySymbol.Add(addressType.Symbol, addressType);
+                m_AddressTypesBytBitset.Add(addressType.GetBitCode().Int, addressType);
+
+                if(addressTypeType == defaultAddressTypeType)
+                    m_DefaultAddressType = addressType;
+            }
+
+            if(null == m_DefaultAddressType)
+                m_DefaultAddressType = Utilities.Reflection.CreateObjectOfType<AddressTypeBase>(defaultAddressTypeType.Name);
+        }
+
+        static AddressTypes()
+        {
+            BuildAddressTypesDatabase();
+        }
+
+        public static AddressTypeBase GetAddressTypeOfSymbol(string symbol)
+        {
+            if(symbol.Equals(""))
+                return m_DefaultAddressType;
+
+            if(symbol.Length > 1)
+                return null;
+
+            return GetAddressTypeOfSymbol(symbol[0]);
+        }
+
+        public static AddressTypeBase GetAddressTypeOfSymbol(char symbol)
+        {
+            if(!m_AddressTypesBySymbol.ContainsKey(symbol))
+                return null;
+
+            return m_AddressTypesBySymbol[symbol];
+        }
+
+        public static AddressTypeBase GetAddressTypeOfBitSet(long value)
+        {
+            if(!m_AddressTypesBytBitset.ContainsKey(value))
+                return null;
+
+            return m_AddressTypesBytBitset[value];
+        }
+    }
+
+    public abstract class AddressTypeBase
+    {
+        public abstract char Symbol { get; }
+        public abstract BitSet GetBitCode();
+
+        public abstract int GetAbsoluteIndex(MemoryModel model, int addressValue);
+        public abstract int PeekAbsoluteIndex(MemoryModel model, int addressValue);
+    }
+
+}
---
+++ Assets/_VirtualMachine/_AddressTypes/AddressTypesImplementation.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VirtualMachine
+{
+    internal class AddressBitCodesDistributor
+    {
+        private static AddressBitCodesDistributor s_Instance = null;
+
+        public static AddressBitCodesDistributor Instance
+        {
+            get
+            {
+                if(null == s_Instance)
+                    s_Instance = new AddressBitCodesDistributor();
+
+                return s_Instance;
+            }
+        }
+
+        private long m_LastGeneratedBitCode = 0;
+
+        public BitSet GenerateFreeBitCode()
+        {
+            var result = new BitSet(MachineConstants.AddressTypeBits);
+            result.Int = m_LastGeneratedBitCode++;
+            return result;
+        }
+    }
+
+    public abstract class SimpleAddressType : AddressTypeBase
+    {
+        private BitSet m_BitCode = null;
+
+        public override BitSet GetBitCode()
+        {
+            if(null == m_BitCode)
+                m_BitCode = AddressBitCodesDistributor.Instance.GenerateFreeBitCode();
+
+            return m_BitCode;
+        }
+    }
+
+    public class ImmediateAddressType : SimpleAddressType
+    {
+        public override char Symbol { get { return '#'; } }
+
+        public override int GetAbsoluteIndex(MemoryModel model, int addressValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BitSet GetBitCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int PeekAbsoluteIndex(MemoryModel model, int addressValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
