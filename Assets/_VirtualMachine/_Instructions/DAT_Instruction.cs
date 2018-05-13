752350f63124d7b5747a6a6047e38c1d36238596
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2018-05-13 13:37:13 +0200

    [Instructions] DAT implementation

---
+++ Assets/_VirtualMachine/_Instructions/DAT_Instruction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VirtualMachine;
+
+public class DAT_Instruction : VirtualMachine.InstructionBase
+{
+    public override ExecutionResult Execute(Process process, MemoryModel model, int address)
+    {
+        return new ExecutionResult(ExecutionResult.ResultType.InvalidOpCode);
+    }
+
+    public override string GetPneumonic()
+    {
+        return "DAT";
+    }
+}
